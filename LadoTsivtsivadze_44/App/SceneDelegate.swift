aa8a3add6e38c1ba1b8103e70c46672ddca3a977
lado at luka <tsivtsivadzelado@yahoo.com>
2021-07-01 01:19:14 -0700

    edited scene delegate

--- LadoTsivtsivadze_44/App/SceneDelegate.swift
+++ LadoTsivtsivadze_44/App/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        configureRootVC(with: scene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -49,6 +50,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Save changes in the application's managed object context when the application transitions to the background.
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+    
+    func configureRootVC(with scene: UIWindowScene) {
+        window = UIWindow(windowScene: scene)
+        coordinator = AppCoordinator(window, navigationController: UINavigationController())
+        coordinator?.start()
     }
 
 
